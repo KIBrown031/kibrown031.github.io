b1f6d6f630b7f99802a5985193944f3366b5f094
KIBrown031 <143941166+KIBrown031@users.noreply.github.com>
2024-11-26 07:17:31 -0500

    commit

--- kibrown031.js
+++ kibrown031.js
@@ -1,1 +1,22 @@
+let topofpage = document.querySelector('.fixed-top');
+let currentDate = new Date;
+topofpage.innerHTML = `${currentDate.toLocaleString()}`;   
 
+
+
+// setTimeout()   setInterval()
+document.body.addEventListener( "keydown", (event) => { console.log(`keydown: ${event.key}`); } );
+
+function updateDate () {
+  let datePicker = document.querySelector('.dateclass');
+  
+  document.querySelector('.date-value').innerHTML = datePicker.value;
+
+  datePicker.addEventListener( "change", () => { console.log("change made to datePicker"); } )
+}
+
+function startup () {
+  console.log('startup');
+  document.querySelector('.dateclass').click();
+  document.querySelector('.dateclass').showPicker();
+}
