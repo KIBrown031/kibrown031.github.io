7e8bffe5641d58e1749013a59dce1fa3e2fc0d15
KIBrown031 <143941166+KIBrown031@users.noreply.github.com>
2024-11-28 06:44:20 -0500

    added functions

---
+++ functions.js
@@ -0,0 +1,10 @@
+function loadBackend() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
