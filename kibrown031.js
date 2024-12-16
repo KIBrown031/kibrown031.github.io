c9d1a409f463f5d69121f252b3391925c73ab2eb
KIBrown031 <143941166+KIBrown031@users.noreply.github.com>
2024-12-16 08:34:25 -0500

    move functions before document ready

--- kibrown031.js
+++ kibrown031.js
@@ -3,6 +3,33 @@ let topofpage = document.querySelector('.div-date-value');
 let currentDate = new Date;
 topofpage.innerHTML = `${currentDate.toLocaleString()}`;   
 
+// setTimeout()   setInterval()
+document.body.addEventListener( "keydown", (event) => { console.log(`keydown: ${event.key}`); } );
+
+function updateDate () {
+  let datePicker = document.querySelector('.input-dateclass');
+  
+  document.querySelector('.div-date-value').innerHTML = datePicker.value;
+
+  datePicker.addEventListener( "change", () => { console.log("change made to datePicker"); } )
+}
+
+function clickDatePicker () {
+  console.log('clickDatePicker');
+  // document.querySelector('.input-dateclass').click();
+  
+  document.querySelector('.input-dateclass').showPicker();
+  
+}
+
+function loadjQueryInDevtools () { // paste directly into console
+  var jq = document.createElement('script');
+  jq.src = "//ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+  document.getElementsByTagName('head')[0].appendChild(jq);
+  jQuery.noConflict();
+}
+
+
 let fz = 14;
 
 $(document).ready(function() {
@@ -150,30 +177,6 @@ $(document).ready(function() {
 
 
 
-      // setTimeout()   setInterval()
-      document.body.addEventListener( "keydown", (event) => { console.log(`keydown: ${event.key}`); } );
-
-      function updateDate () {
-        let datePicker = document.querySelector('.input-dateclass');
-        
-        document.querySelector('.div-date-value').innerHTML = datePicker.value;
-
-        datePicker.addEventListener( "change", () => { console.log("change made to datePicker"); } )
-      }
-
-      function clickDatePicker () {
-        console.log('clickDatePicker');
-        // document.querySelector('.input-dateclass').click();
-        
-        document.querySelector('.input-dateclass').showPicker();
-      }
-
-      function loadjQueryInDevtools () { // paste directly into console
-        var jq = document.createElement('script');
-        jq.src = "//ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
-        document.getElementsByTagName('head')[0].appendChild(jq);
-        jQuery.noConflict();
-      }
 
   
 });
@@ -183,4 +186,6 @@ $.getJSON(url,data, successCallbacK)
 .done(function(){})
 .fail(function(){})
 .always(function(){});
+
+alert()  prompt()  confirm()
 */
