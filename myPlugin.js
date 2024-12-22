5a1aac39cf0a1172c9e0e26b8f49de8f0a08b87a
KIBrown031 <143941166+KIBrown031@users.noreply.github.com>
2024-12-21 22:04:57 -0500

    added myPlugin.js

--- myPlugin.js
+++ myPlugin.js
@@ -54,3 +54,50 @@
  
 // Usage example:
 // $( "a" ).showLinkLocation();
+
+
+let arr = ['hello', 'from', 'easy', 'programming', '.net'];
+//listThis()
+(function( $ ) {
+
+    $.fn.listThis = function(listType) {
+        let l =  $('<' + listType + '>');
+    
+    this.each(function(i, item) {
+        $(l).append($('<li>').append(item));
+    });
+    
+    return l;
+    }
+
+}( jQuery ));
+
+
+//colorMyList()
+(function( $ ) {
+
+    $.fn.colorMyList = function(options) {
+        let settings = $.extend({
+        color: '#000',
+        fontSize: '16px',
+        textdecoration: 'underline'
+    }, options);
+    
+    return this.css({
+        'color': settings.color,
+        'font-size': settings.fontSize,
+        'text-decoration': settings.textdecoration
+    });
+    }
+
+}( jQuery ));
+
+
+
+$('#result').append($(arr).listThis('ul').colorMyList(
+	{
+	color: '#0000ff',
+    fontSize: '30px',
+    textDecoration: 'none'
+	}
+));
