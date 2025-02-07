c18b2bcedaf3f7b3eeaf3ed172ebc6a2edb5cc17
KIBrown031 <143941166+KIBrown031@users.noreply.github.com>
2025-02-07 07:45:04 -0500

    json

--- kibrown031.js
+++ kibrown031.js
@@ -241,6 +241,7 @@ $(document).ready(function() {
         let regexAlpha = /^[^A-Za-z].*$/;  // must start with a letter, not a period .
         let regexTrue = /^true$/;           // 'true' last column for elder and pioneer
 
+        window.location.href = "http://127.0.0.1:3000/json/notesdb.json"
         window.location.href = "/json/notesdb.json"
         // alert(`/json/${selectedfile}`);
 
