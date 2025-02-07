14b4e53057b85027116dcb42343720879c625266
KIBrown031 <143941166+KIBrown031@users.noreply.github.com>
2025-02-07 06:45:04 -0500

    json

--- kibrown031.js
+++ kibrown031.js
@@ -241,7 +241,7 @@ $(document).ready(function() {
         let regexAlpha = /^[^A-Za-z].*$/;  // must start with a letter, not a period .
         let regexTrue = /^true$/;           // 'true' last column for elder and pioneer
 
-        // alert(`/json/${selectedfile}`);
+        alert(`/json/${selectedfile}`);
 
         $.getJSON(`/json/${selectedfile}`, function(data) {  // junk.json contains 'secrets'
           
